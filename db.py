7a9a79d95b12e6a8227a1d7783c169b4bc8850d8
Uddeshya1994 <69723139+Uddeshya1994@users.noreply.github.com>
2026-01-07 00:06:06 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
-import psycopg2
 import streamlit as st
+import psycopg
 
 def get_connection():
-    return psycopg2.connect(
+    return psycopg.connect(
         host=st.secrets["DB_HOST"],
-        database=st.secrets["DB_NAME"],
+        dbname=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
         port=st.secrets["DB_PORT"],
-        sslmode="require"   # ✅ THIS IS CRITICAL FOR SUPABASE
+        sslmode="require"
     )
