e2da0461485725c8edea34b2447fa19c63b60d9f
Uddeshya1994 <69723139+Uddeshya1994@users.noreply.github.com>
2026-01-07 00:26:33 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,11 +2,4 @@ import streamlit as st
 import psycopg
 
 def get_connection():
-    return psycopg.connect(
-        host=st.secrets["DB_HOST"],
-        dbname=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        port=st.secrets["DB_PORT"],
-        sslmode="require"
-    )
+    return psycopg.connect(st.secrets["DATABASE_URL"])
